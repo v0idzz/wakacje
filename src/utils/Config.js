48d4fa2a7aa3f851cc3a9c41370c3263da5164ab
Piotr Romanowski <piotr@romanowski.me>
2020-09-01 12:10:27 +0200

    Update dates

--- src/utils/Config.js
+++ src/utils/Config.js
@@ -1,123 +1,111 @@
 export const authorWebsite = 'https://romanowski.me/';
 
-export const VacationStart = '27-06-2020';
-export const SchoolStart = '02-09-2019';
+export const VacationStart = '26-06-2021';
+export const SchoolStart = '01-09-2020';
 
 export const Holidays = [
   {
-    date: '14-10-2019',
+    date: '14-10-2020',
     name: 'Dzień Edukacji Narodowej'
   },
   {
-    date: '01-11-2019',
-    name: 'Wszystkich Świętych'
-  },
-  {
-    date: '11-11-2019',
+    date: '11-11-2020',
     name: 'Narodowe święto Niepodległości'
   },
   {
-    date: '23-12-2019',
+    date: '23-12-2020',
     name: 'Przerwa świąteczna'
   },
   {
-    date: '24-12-2019',
+    date: '24-12-2020',
     name: 'Wigilia Bożego Narodzenia'
   },
   {
-    date: '25-12-2019',
+    date: '25-12-2020',
     name: 'Boże Narodzenie (dzień pierwszy)'
   },
   {
-    date: '26-12-2019',
-    name: 'Boże Narodzenie (dzień drugi)'
-  },
-  {
-    date: '27-12-2019',
+    date: '28-12-2020',
     name: 'Przerwa świąteczna'
   },
   {
-    date: '30-12-2019',
+    date: '29-12-2020',
     name: 'Przerwa świąteczna'
   },
   {
-    date: '31-12-2019',
+    date: '30-12-2020',
+    name: 'Przerwa świąteczna'
+  },
+  {
+    date: '31-12-2020',
     name: 'Sylwester'
   },
   {
-    date: '01-01-2020',
+    date: '01-01-2021',
     name: 'Nowy Rok'
   },
   {
-    date: '06-01-2020',
+    date: '06-01-2021',
     name: 'Trzech Króli'
   },
   {
-    date: '13-01-2020',
+    date: '01-02-2021',
     name: 'Ferie zimowe'
   },
   {
-    date: '14-01-2020',
+    date: '02-02-2021',
     name: 'Ferie zimowe'
   },
   {
-    date: '15-01-2020',
+    date: '03-02-2021',
     name: 'Ferie zimowe'
   },
   {
-    date: '16-01-2020',
+    date: '04-02-2021',
     name: 'Ferie zimowe'
   },
   {
-    date: '17-01-2020',
+    date: '05-02-2021',
     name: 'Ferie zimowe'
   },
   {
-    date: '20-01-2020',
+    date: '08-02-2021',
     name: 'Ferie zimowe'
   },
   {
-    date: '21-01-2020',
+    date: '09-02-2021',
     name: 'Ferie zimowe'
   },
   {
-    date: '22-01-2020',
+    date: '10-02-2021',
     name: 'Ferie zimowe'
   },
   {
-    date: '23-01-2020',
+    date: '11-02-2021',
     name: 'Ferie zimowe'
   },
   {
-    date: '24-01-2020',
+    date: '12-02-2021',
     name: 'Ferie zimowe'
   },
   {
-    date: '09-04-2020',
+    date: '01-04-2021',
     name: 'Wielki Czwartek'
   },
   {
-    date: '10-04-2020',
+    date: '02-04-2021',
     name: 'Wielki Piątek'
   },
   {
-    date: '11-04-2020',
-    name: 'Wielka Sobota'
-  },
-  {
-    date: '12-04-2020',
-    name: 'Wielkanoc'
-  },
-  {
-    date: '13-04-2020',
+    date: '05-04-2021',
     name: 'Poniedziałek Wielkanocny'
   },
   {
-    date: '01-05-2020',
-    name: 'Międzynarodowe Święto Pracy'
+    date: '03-05-2021',
+    name: 'Święto Konstytucji 3 Maja'
   },
   {
-    date: '11-06-2020',
+    date: '03-06-2021',
     name: 'Boże Ciało'
   }
 ];
